cb720c740680a2e277658f9d6590f19a90ff86f6
Nokukhanya Makhanya <nokhanyamakhanya@gmail.com>
2022-02-24 21:41:38 +0200

    CSS styling

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
-
 const buttonElem = document.querySelector(".btn");
+const buttonElem1 = document.querySelector(".btn1");
 const msgBox = document.querySelector(".message");
+const newGameMsg = document.querySelector(".newGameMsg")
 
-let randomNumber = Math.ceil((Math.random() * 100))
-// var guessNumber = inputBox.value;
+let randomNumber = Math.ceil((Math.random() * 100));
 
 
+buttonElem.addEventListener("click",function(){ 
+    comparedGuess();
+});
 
-buttonElem.addEventListener("click",function(){
-    
-    comparedGuess(); 
-    
-    // if(guessNumber>randomNumber){
-    //     msgBox.innerHTML= 'Your guess is too high';
-    // }
-    // else if(guessNumber<randomNumber){
-    //     msgBox.innerHTML= 'Your guess is too low';
-    // }
-    // else{
-    //     msgBox.innerHTML=`Correct, the secret number is ${guessNumber}`
-    // }
-    
+buttonElem1.addEventListener("click",function(){ 
+    location.reload()
 });
 
 
 function comparedGuess(){
     let guessNumber = document.getElementById("inputBox").value;
-    if(guessNumber>randomNumber){
-        msgBox.innerHTML= 'Your guess is too high';
-    }
-    else if(guessNumber<randomNumber){
-        msgBox.innerHTML= 'Your guess is too low';
-    }
-    else{
-        msgBox.innerHTML=`Correct, the secret number is ${guessNumber}`
+    
+    if(guessNumber<1 || guessNumber>100){
+        msgBox.innerHTML= 'You must enter values between 0 and 100'
+    }else{
+        if(guessNumber>randomNumber){
+            msgBox.innerHTML= 'Your guess is too high'+ randomNumber;
+            msgBox.style.color='red'
+        }
+        else if(guessNumber<randomNumber){
+            msgBox.innerHTML= 'Your guess is too low'+ randomNumber;
+            msgBox.style.color='red'
+        }
+        else{
+            msgBox.innerHTML=`Correct, the secret number is ${guessNumber}`
+            msgBox.style.color='green';
+            setTimeout(function(){newGameMsg.classList.remove("hidden");}, 2000);
+            setTimeout(function(){location.reload()}, 5000);  
+        }  
     }  
 }
+
+
 
 
